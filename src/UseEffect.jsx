9181a4e1e98070af8c933f3da6cc8f0ac128405a
Saif <saifuzzamankhan53@gmail.com>
2022-07-06 17:49:22 +0530

    first commit

---
+++ src/UseEffect.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+
+const UseEffect = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/albums/1/photos");
+        setUsers(await response.json());
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+
+
+    return (
+        <>
+            <h2 style={{textAlign:'center', color:'red', textDecoration:'underline'}}>List of Github Users</h2>
+            <div className="container-fluid mt-3">
+                <div className="row text-center">
+                    {
+                        users.map((curElem) => {
+
+                            return (
+                                <div className="col-10 col-md-4 mt-5" key={curElem.id}>
+                                    <div className="card p-2">
+                                        <div className="d-flex align-items-center">
+                                            <div className="image"><img src={curElem.description} class="rounded" alt="img" width="155" /></div>
+                                            <div className="ml-3 w-100">
+                                                <h4 className="mb-0 mt-0 textLeft">Saif</h4>
+                                                <span className="textLeft">{curElem.title}</span>
+                                                <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                                                    <div className="d-flex flex-column">
+                                                        <span className="articles">Articles</span><span className="number1">38</span></div>
+                                                    <div className="d-flex flex-column">
+                                                        <span className="Followers">Followers</span><span className="number2">980</span></div>
+                                                    <div className="d-flex flex-column">
+                                                        <span className="rating">Rating</span><span className="number3">8.9</span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+        </>
+
+    )
+}
+
+
+
+export default UseEffect;
